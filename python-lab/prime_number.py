23a3723a7f2126345b7ba1ce49d5e7ef64808e96
iamvickypedia <mailvikastiwari@gmail.com>
2019-04-26 10:05:57 +0000

    Update prime_number.py

--- python-lab/prime_number.py
+++ python-lab/prime_number.py
@@ -2,29 +2,28 @@
 # we also print the milliseconds required to run this program
 import time
 
-def checkPrime(n) : 
-    start = time.time()
-    if (n <= 1) : 
+def checkPrime(n) :
+    if (n <= 1) :
         return False
-    if (n <= 3) : 
+    if (n <= 3) :
         return True
-  
-    if (n % 2 == 0 or n % 3 == 0) : 
+
+    if (n % 2 == 0 or n % 3 == 0) :
         return False
-  
+
     i = 5
-    while(i * i <= n) : 
-        if (n % i == 0 or n % (i + 2) == 0) : 
+    while(i * i <= n) :
+        if (n % i == 0 or n % (i + 2) == 0) :
             return False
         i = i + 6
-    print('Time taken to conclude : {}'.format(round((time.time() - start)*100)))
     return True
-  
-  
-in_number = input("Enter Number to check Prime\n")
-is_prime = isPrime(int(in_number))
 
-if (is_prime) : 
-    print("{} is Prime Number".format(in_number)) 
-else : 
+
+in_number = input("Enter Number to check Prime\n")
+st = time.time()
+is_prime = checkPrime(int(in_number))
+print('Time taken to run is {}'.format(time.time() - st))
+if (is_prime) :
+    print("{} is a Prime Number".format(in_number))
+else :
     print("{} is not a Prime Number".format(in_number))
