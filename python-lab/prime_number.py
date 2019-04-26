5c1db17455fd1cae03dcd0684d0e61510ba9e7bb
iamvickypedia <mailvikastiwari@gmail.com>
2019-04-26 09:49:55 +0000

    Update prime_number.py

--- python-lab/prime_number.py
+++ python-lab/prime_number.py
@@ -1,6 +1,9 @@
 #This is an optimised function to check for prime number using 6k + i concept
+# we also print the milliseconds required to run this program
+import time
+
 def checkPrime(n) : 
-  
+    start = time.time()
     if (n <= 1) : 
         return False
     if (n <= 3) : 
@@ -14,7 +17,7 @@ def checkPrime(n) :
         if (n % i == 0 or n % (i + 2) == 0) : 
             return False
         i = i + 6
-  
+    print('Time taken to conclude : {}'.format(round((time.time() - start)*100)))
     return True
   
   
