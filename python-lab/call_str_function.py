75e2bbd4c182ed3146adcf8eeb04288acd2792ed
iamvickypedia <mailvikastiwari@gmail.com>
2019-04-26 13:10:27 +0000

    Add new file

---
+++ python-lab/call_str_function.py
@@ -0,0 +1,15 @@
+#this program deals with strings and then calls the functions which remembles those string literals
+
+
+def A():
+    print('A')
+    
+def B():
+    print('B')
+    
+
+caller = lambda x : globals()[x]()
+
+calling_str = input(' A\n B\nEnter Function Name\n')
+caller(calling_str)
+
