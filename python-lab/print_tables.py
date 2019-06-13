7d6bba56501f9e5e4eab4a7e5e29a159c6b3c523
iamvickypedia <mailvikastiwari@gmail.com>
2019-06-13 14:05:17 +0000

    Tables file

---
+++ python-lab/print_tables.py
@@ -0,0 +1,8 @@
+def print_table(n):
+    for i in range(1,11):
+        print(n,' * ',i,' = ',i*n)
+      
+print('Welcome to Tables generator. Tell your teachers the table of any number in a second')
+inp = input('Enter the number to generate its table')
+
+print_table(int(inp))
