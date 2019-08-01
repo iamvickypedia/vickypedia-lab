2ff771049be774d0db39fddf0ef95a458591d717
vikas.t <vikas.tiwari@crimsoni.com>
2019-08-01 20:50:32 +0530

    formatting changes

--- python-lab/coin-toss.py
+++ python-lab/coin-toss.py
@@ -1,10 +1,12 @@
 import random
 
-sample_set = ['Heads','Tails']
+sample_set = ['Heads', 'Tails']
+
 
 def get_res():
-	return random.choice(sample_set)
+    return random.choice(sample_set)
+
 
 input("Welcome to the Coin Toss Program.\nPress ENTER to toss the coin and see the result")
 
-print("\n\n"+get_res()+ "\n\n")
+print("\n\n" + get_res() + "\n\n")
