c7c8e7fe64eb839b2e36aac559e2fbaadd91deb1
iamvickypedia <mailvikastiwari@gmail.com>
2019-08-17 20:25:22 +0530

    docstring and changes

--- python-lab/reverse-integer.py
+++ python-lab/reverse-integer.py
@@ -1,4 +1,9 @@
 def reverse_int(inp):
+	"""
+	This is a program which takes an intreger and reverses the number by having some calculation based operation.
+	We can also easily reverse a number by converting it to string and returning the reversed string which you can convert to number again.
+	But this is an approach circumventiung that hack.
+	"""
 	newnum = 0
 	if inp.isdigit():
 		digits = len(inp) - 1
--- python-lab/reverse-string.py
+++ python-lab/reverse-string.py
@@ -1,13 +1,12 @@
-# This program would contain a function which contains the string as a parameter and then returns the reversed string 
-
 def reverse_string(str):
+    """
+    This is a program which takes a string and reverses the string by having for loop based operation.
+    """
     temp_str = ""
     for chr in str:
         temp_str = chr + temp_str
 
     return temp_str
 
-string_l = 'Lorem ipsum dolor sit amet'
-print(string_l)
+string_l = input("Enter the string to reverse\n")
 print(reverse_string(string_l))
-
